d9e0b2c260b9f400c78faee4745ea4d150fafe88
formalkunal <formal.kunal@gmail.com>
2025-12-20 23:03:57 +0530

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,33 @@
+package com.guvi.calorie;
+
+public class Food {
+    private String name;
+    private int calories;
+    private int protein;
+    private int carbs;
+    private int fats;
+
+    public Food(String name, int calories, int protein, int carbs, int fats) {
+        this.name = name;
+        this.calories = calories;
+        this.protein = protein;
+        this.carbs = carbs;
+        this.fats = fats;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public int getProtein() {
+        return protein;
+    }
+
+    public int getCarbs() {
+        return carbs;
+    }
+
+    public int getFats() {
+        return fats;
+    }
+}
